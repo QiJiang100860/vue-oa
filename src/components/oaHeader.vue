ac32cb3c32a816931270cb08bddeba589f58fe8e
jiangqi <18710609128@163.com>
2018-04-27 18:12:31 +0800

    调整header

--- src/components/oaHeader.vue
+++ src/components/oaHeader.vue
@@ -2,9 +2,9 @@
   <el-header class="oa-header">
     <el-row :gutter="22">
       <el-col :span="2">后台管理系统</el-col>
-      <el-col :span="1" :offset="21">
-        <el-dropdown>
-          <div class="el-dropdown-link">
+      <el-col :span="1" :offset="19">
+        <el-dropdown class="el-dropdown-headicon-wrapper">
+          <div class="el-dropdown-headicon">
             <img src="" alt="">
           </div>
           <el-dropdown-menu slot="dropdown">
@@ -13,6 +13,7 @@
           </el-dropdown-menu>
         </el-dropdown>
       </el-col>
+      <el-col  class="el-dropdown-time" :span="2"><span>2018-04-27</span><span class="lastspan">17:57</span></el-col>
     </el-row>
   </el-header>
 </template>
@@ -39,28 +40,34 @@ export default {
       width: 100%;
       
     }
-    .el-dropdown-link{
-      color: #fff;
-      cursor: pointer;
-      line-height: 50px;
-      height:50px;
-      overflow: hidden;
-      img{
-        display: inline-block;
-        width: 36px;
-        height: 36px;
-        border-radius: 18px;
-        box-sizing: border-box;
-        border: solid 1px #dedede;
-        margin-top: 7px;
-        margin-right: 10px;
+    .el-dropdown-headicon-wrapper{
+      margin-left: 30px;
+      .el-dropdown-headicon{
+        color: #fff;
+        cursor: pointer;
+        line-height: 50px;
+        height:50px;
+        overflow: hidden;
+        img{
+          display: inline-block;
+          width: 36px;
+          height: 36px;
+          border-radius: 18px;
+          box-sizing: border-box;
+          border: solid 1px #dedede;
+          margin: 0 auto;
+          margin-top: 7px;
+        }
+        
       }
+    }
+    .el-dropdown-time{
       span{
         font-size: 14px;
         color: #fff;
         vertical-align: 13px;
         &.lastspan{
-          margin-left: 20px;
+          margin-left: 10px;
         }
       }
     }
